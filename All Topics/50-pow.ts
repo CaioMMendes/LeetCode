e9fb790fafb4b25638249cdfc5859334968994ba
Caio Martins Mendes Rodrigues <caio03martins@gmail.com>
2024-01-29 13:15:32 -0300

    Add 50 and 67 All topics exercises

---
+++ All Topics/50-pow.ts
@@ -0,0 +1,8 @@
+const x = 2.1,
+  n = 3;
+
+var myPow = function (x: number, n: number) {
+  return x ** n;
+};
+
+console.log(myPow(x, n));
---
+++ All Topics/67-addBinarys.ts
@@ -0,0 +1,14 @@
+const a =
+    "10100000100100110110010000010101111011011001101110111111111101000000101111001110001111100001101",
+  b =
+    "110101001011101110001111100110001010100001101011101010000011011011001011101111001100000011011110011";
+
+const addBinary = function (a: string, b: string) {
+  const binary1 = BigInt(`0b${a}`);
+  const binary2 = BigInt(`0b${b}`);
+
+  const sum = binary1 + binary2;
+  return BigInt(sum).toString(2);
+};
+
+console.log(addBinary(a, b));
