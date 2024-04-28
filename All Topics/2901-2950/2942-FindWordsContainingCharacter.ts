e65841940ba5a7b78cc622b8936c89a686b8883a
Caio Martins Mendes Rodrigues <caio03martins@gmail.com>
2024-04-28 20:01:36 -0300

    Add 2942 All Topics exercises

---
+++ All Topics/2901-2950/2942-FindWordsContainingCharacter.ts
@@ -0,0 +1,22 @@
+const words = ["leet", "code"],
+  x = "e"
+
+/**
+ * @param {string[]} words
+ * @param {character} x
+ * @return {number[]}
+ */
+var findWordsContaining = function (words: string[], x: string) {
+  const result = []
+
+  words.forEach((string, index) => {
+    const charIndex = string.indexOf(x)
+    if (charIndex !== -1) {
+      result.push(index)
+    }
+  })
+
+  return result
+}
+
+console.log(findWordsContaining(words, x))
