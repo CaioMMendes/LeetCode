47cb4fd49de769508370b7ee829aa6d7a1946e2b
Caio Martins Mendes Rodrigues <caio03martins@gmail.com>
2024-05-17 15:59:14 -0300

    Add 344 All Topics exercises

---
+++ All Topics/301-350/344-ReverseString.ts
@@ -0,0 +1,11 @@
+const s = ["h", "e", "l", "l", "o"]
+
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function (s: string[]) {
+  return s.reverse()
+}
+
+console.log(reverseString(s))
