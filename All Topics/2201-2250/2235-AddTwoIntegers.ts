4ba05a5d5364d21f887bc31f63e6a46c8dc1e668
Caio Martins Mendes Rodrigues <caio03martins@gmail.com>
2024-06-15 23:38:37 -0300

    Add 2235 All Topics exercises

---
+++ All Topics/2201-2250/2235-AddTwoIntegers.ts
@@ -0,0 +1,13 @@
+const num1 = -10,
+  num2 = 4
+
+/**
+ * @param {number} num1
+ * @param {number} num2
+ * @return {number}
+ */
+var sum = function (num1: number, num2: number) {
+  return num1 + num2
+}
+
+console.log(sum(num1, num2))
