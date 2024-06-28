b7f7bdb83a565c09f7a7df911120fe4695901097
Caio Martins Mendes Rodrigues <115363966+CaioMMendes@users.noreply.github.com>
2024-06-28 01:17:47 -0300

    Add 504 All Topics exercises

---
+++ All Topics/501-550/504-Base7.ts
@@ -0,0 +1,11 @@
+const num = 100
+
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var convertToBase7 = function (num: number) {
+  return num.toString(7)
+}
+
+console.log(convertToBase7(num))
