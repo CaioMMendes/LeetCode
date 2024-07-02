966e2feed6126ba39e679d6cf3bc7a06902015a4
Caio Martins Mendes Rodrigues <115363966+CaioMMendes@users.noreply.github.com>
2024-07-02 13:55:28 -0300

    Add 1945 All Topics exercises

---
+++ All Topics/1901-1950/1945-SumofDigitsofStringAfterConvert.ts
@@ -0,0 +1,49 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+var getLucky = function (s: string, k: number) {
+  const alphabet = {
+    "a": "1",
+    "b": "2",
+    "c": "3",
+    "d": "4",
+    "e": "5",
+    "f": "6",
+    "g": "7",
+    "h": "8",
+    "i": "9",
+    "j": "10",
+    "k": "11",
+    "l": "12",
+    "m": "13",
+    "n": "14",
+    "o": "15",
+    "p": "16",
+    "q": "17",
+    "r": "18",
+    "s": "19",
+    "t": "20",
+    "u": "21",
+    "v": "22",
+    "w": "23",
+    "x": "24",
+    "y": "25",
+    "z": "26",
+  }
+
+  const splited = s.split("")
+
+  let reduced: string | number = splited.reduce(
+    (acc, cur) => acc + alphabet[cur],
+    ""
+  )
+
+  for (let i = 0; i < k; i++) {
+    const splitedFor = String(reduced).split("")
+    reduced = splitedFor.reduce((acc, cur) => acc + Number(cur), 0)
+  }
+
+  return reduced
+}
