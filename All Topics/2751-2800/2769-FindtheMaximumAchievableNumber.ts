b0423fd762f797125fcd252a310756394c45ab39
Caio Martins Mendes Rodrigues <115363966+CaioMMendes@users.noreply.github.com>
2024-07-11 14:31:15 -0300

    Add 2769 All Topics exercises

---
+++ All Topics/2751-2800/2769-FindtheMaximumAchievableNumber.ts
@@ -0,0 +1,13 @@
+const num = 4,
+  t = 1
+
+/**
+ * @param {number} num
+ * @param {number} t
+ * @return {number}
+ */
+var theMaximumAchievableX = function (num: number, t: number) {
+  return num + t * 2
+}
+
+console.log(theMaximumAchievableX(num, t))
