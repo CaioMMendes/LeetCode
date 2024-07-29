d7202ee00262d2f78507cc92bce1e14c58c81b2f
Caio Martins Mendes Rodrigues <115363966+CaioMMendes@users.noreply.github.com>
2024-07-29 17:44:33 -0300

    Add 1581 All Topics exercises

---
+++ All Topics/1551-1600/1581-CustomerWhoVisited butDidNotMakeAnyTransactions.sql
@@ -0,0 +1,5 @@
+-- Write your PostgreSQL query statement below
+select customer_id, count( * ) as count_no_trans 
+from Visits left join Transactions on Transactions.visit_id = visits.visit_id 
+where  transaction_id is null
+group by customer_id ;
