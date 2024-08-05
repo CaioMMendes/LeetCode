b20642104c2c5d9c02daf7456ca2a5373617b017
Caio Martins Mendes Rodrigues <115363966+CaioMMendes@users.noreply.github.com>
2024-08-05 00:31:15 -0300

    Add 2053 All Topics exercises

---
+++ All Topics/2051-2100/2053-KthDistinctStringinanArray.java
@@ -0,0 +1,27 @@
+class Solution {
+  public String kthDistinct(String[] arr, int k) {
+    LinkedHashMap<String, Integer> map = new LinkedHashMap<>();
+
+    for (String palavra : arr) {
+
+      int element = map.get(palavra) == null ? 0 : map.get(palavra);
+      map.put(palavra, element + 1);
+    }
+    int currentIndex = 0;
+    for (Map.Entry<String, Integer> entry : map.entrySet()) {
+      String chave = entry.getKey();
+      Integer valor = entry.getValue();
+
+      if (valor == 1) {
+        currentIndex++;
+
+      }
+      if (currentIndex == k) {
+        return chave;
+      }
+    }
+
+    return "";
+
+  }
+}
