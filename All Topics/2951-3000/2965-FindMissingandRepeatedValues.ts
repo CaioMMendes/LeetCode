c2b9e1106525b754f1c35f48944d7fb8f9d25f3a
Caio Martins Mendes Rodrigues <115363966+CaioMMendes@users.noreply.github.com>
2024-08-11 20:08:03 -0300

    Add 2965 All Topics exercises

---
+++ All Topics/2951-3000/2965-FindMissingandRepeatedValues.ts
@@ -0,0 +1,37 @@
+const grid = [
+  [1, 3],
+  [2, 2],
+]
+
+/**
+ * @param {number[][]} grid
+ * @return {number[]}
+ */
+var findMissingAndRepeatedValues = function (grid) {
+  const result: number[] = []
+  const elements = {}
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[0].length; j++) {
+      const element = grid[i][j]
+      if (!elements[element]) {
+        elements[element] = true
+      } else {
+        result[0] = element
+      }
+    }
+  }
+  let count = 1
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[0].length; j++) {
+      if (!elements[count]) {
+        result[1] = count
+        return result
+      }
+      count++
+    }
+  }
+
+  return result
+}
+
+console.log(findMissingAndRepeatedValues(grid))
