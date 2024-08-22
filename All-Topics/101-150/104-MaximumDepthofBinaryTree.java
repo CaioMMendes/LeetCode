9ce825a03fa8c86eac2975377c032a23ef548f21
Caio Martins Mendes Rodrigues <115363966+CaioMMendes@users.noreply.github.com>
2024-08-21 22:55:36 -0300

    Add 104 All Topics exercises

---
+++ All-Topics/101-150/104-MaximumDepthofBinaryTree.java
@@ -0,0 +1,43 @@
+import javax.swing.tree.TreeNode;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+
+  int result = 0;
+
+  public int maxDepth(TreeNode root) {
+    int teste = 0;
+
+    findLast(root, teste);
+    return result;
+  }
+
+  private void findLast(TreeNode no, int count) {
+    if (no == null) {
+
+      if (count > result) {
+        result = count;
+      }
+      return;
+    }
+
+    findLast(no.left, count + 1);
+
+    findLast(no.right, count + 1);
+
+  }
+}
