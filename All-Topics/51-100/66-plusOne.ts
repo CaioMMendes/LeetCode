7a1b7a78ce62eb397f02bf5d7587f45a2af847a0
Caio Martins Mendes Rodrigues <115363966+CaioMMendes@users.noreply.github.com>
2024-12-14 01:35:14 -0300

    Add 66 All Topics exercises now returns number

--- All-Topics/51-100/66-plusOne.ts
+++ All-Topics/51-100/66-plusOne.ts
@@ -1,13 +1,24 @@
-const digits = [6, 1, 4, 5, 3, 9, 0, 1, 9, 5, 1, 8, 6, 7, 0, 5, 5, 4, 3];
+const digits = [6, 1, 4, 5, 3, 9, 0, 1, 9, 5, 1, 8, 6, 7, 0, 5, 5, 4, 3]
 
-const plusOne = function (digits: number[]) {
-  const string = digits.join("");
+// const plusOne = function (digits: number[]) {
+//   const string = digits.join("");
 
-  //todo precisa usar BigInt porque quando o número é muito grande o js perde precisão
-  //todo e depois de um certo numero de casas ele coloca tudo 0
-  const number = BigInt(string) + BigInt(1);
-  const array = String(number).split("");
-  return array;
-};
+//   //todo precisa usar BigInt porque quando o número é muito grande o js perde precisão
+//   //todo e depois de um certo numero de casas ele coloca tudo 0
+//   const number = BigInt(string) + BigInt(1);
+//   const array = String(number).split("");
+//   return array;
+// };
 
-console.log(plusOne(digits));
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function (digits: number[]) {
+  const string = digits.join("")
+  const number = BigInt(string) + BigInt(1)
+  const array = String(number).split("")
+  return array.map(Number)
+}
+
+console.log(plusOne(digits))
