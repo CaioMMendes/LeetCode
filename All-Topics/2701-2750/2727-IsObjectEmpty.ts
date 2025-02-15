c2f2489ed327796a213990e7db0435b91e4716e9
Caio Martins Mendes Rodrigues <115363966+CaioMMendes@users.noreply.github.com>
2025-02-14 21:52:59 -0300

    fix: 2727 type

--- All-Topics/2701-2750/2727-IsObjectEmpty.ts
+++ All-Topics/2701-2750/2727-IsObjectEmpty.ts
@@ -4,7 +4,7 @@ const obj = { "x": 5, "y": 42 }
  * @param {Object|Array} obj
  * @return {boolean}
  */
-var isEmpty = function (obj) {
+var isEmpty = function (obj: any) {
   return Object.keys(obj).length === 0
 }
 
