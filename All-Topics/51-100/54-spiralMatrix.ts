23a1f5b07017d28c134090b48b936a55199ac650
Caio Martins Mendes Rodrigues <115363966+CaioMMendes@users.noreply.github.com>
2025-11-26 18:26:59 -0300

    Add another way to solve 54 problem

--- All-Topics/51-100/54-spiralMatrix.ts
+++ All-Topics/51-100/54-spiralMatrix.ts
@@ -1,42 +1,84 @@
 // const matrix = [[7], [9], [6]];
-const matrix = [[1], [2], [3], [4], [5], [6], [7], [8], [9], [10]];
+const matrix = [[1], [2], [3], [4], [5], [6], [7], [8], [9], [10]]
 // const matrix = [
 //   [1, 2, 3, 4],
 //   [5, 6, 7, 8],
 //   [9, 10, 11, 12],
 //   [13, 14, 15, 16],
 // ];
-console.log("------------------------");
+console.log("------------------------")
 var spiralOrder = function (matrix: number[][]) {
-  const array: number[] = [];
-  const matrixXlength = matrix[0].length;
-  const matrixYlength = matrix.length;
-  console.log(matrixXlength, matrixYlength);
+  const array: number[] = []
+  const matrixXlength = matrix[0].length
+  const matrixYlength = matrix.length
+  console.log(matrixXlength, matrixYlength)
   for (let vezes = 0; vezes < Math.ceil(matrix.length / 2); vezes++) {
     for (let i = 0; i < matrixXlength - vezes * 2; i++) {
-      array.push(matrix[vezes][i + vezes]);
+      array.push(matrix[vezes][i + vezes])
     }
     if (Math.ceil(matrixXlength / 2) > vezes) {
       for (let i = 0; i < matrixYlength - 1 - vezes * 2; i++) {
-        console.log("first");
-        array.push(matrix[i + vezes + 1][matrixXlength - 1 - vezes]);
+        console.log("first")
+        array.push(matrix[i + vezes + 1][matrixXlength - 1 - vezes])
       }
     }
     if (Math.floor(matrixYlength / 2) > vezes) {
       for (let i = 0; i < matrixXlength - 1 - vezes * 2; i++) {
         array.push(
           matrix[matrixYlength - 1 - vezes][matrixXlength - 1 - vezes - i - 1]
-        );
+        )
       }
     }
     if (Math.floor(matrixXlength / 2) > vezes) {
       for (let i = 0; i < matrixYlength - 2 - vezes * 2; i++) {
-        array.push(matrix[matrixYlength - 1 - vezes - 1 - i][vezes]);
+        array.push(matrix[matrixYlength - 1 - vezes - 1 - i][vezes])
       }
     }
-    console.log(array);
+    console.log(array)
   }
-  return array;
-};
+  return array
+}
 
-console.log(spiralOrder(matrix));
+console.log(spiralOrder(matrix))
+
+//? Outra forma de resolver
+// /**
+//  * @param {number[][]} matrix
+//  * @return {number[]}
+//  */
+// var spiralOrder = function(matrix) {
+
+//     const result=[]
+
+//     while(matrix.length>0){
+//         result.push(...matrix.splice(0,1)[0])
+
+//         for(let i=0;i<matrix.length;i++){
+
+//             const last=matrix[i].pop()
+//             if(matrix[i].length===0){
+//                 matrix.splice(i,1)
+//                 i--
+//             }
+//             result.push(last)
+//         }
+//         if(matrix.length>0){
+//             const reversed=matrix.splice(matrix.length-1,1)[0].reverse()
+//             result.push(...reversed)
+//         }
+
+//         if(matrix.length>0){
+//             for(let i=matrix.length-1;i>=0;i--){
+//             const first=matrix[i].shift()
+//             if(matrix[i].length===0){
+//                 matrix.splice(i,1)
+//             }
+//             result.push(first)
+//             }
+//         }
+
+//     }
+
+//     return result
+
+// };
