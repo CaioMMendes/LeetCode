53d349d5bbe6f8542691135fa45628662b059e99
Caio Martins Mendes Rodrigues <115363966+CaioMMendes@users.noreply.github.com>
2025-12-07 18:42:54 -0300

    Add 121 All Topics exercises using pointers

--- All-Topics/101-150/121-BestTimetoBuyandSellStock.ts
+++ All-Topics/101-150/121-BestTimetoBuyandSellStock.ts
@@ -1,26 +1,55 @@
-const prices = [7, 6, 4, 3, 1];
+const prices = [7, 6, 4, 3, 1]
 
 /**
  * @param {number[]} prices
  * @return {number}
  */
 const maxProfit = (prices: number[]) => {
-  let min = prices[0];
-  let result = 0;
+  let min = prices[0]
+  let result = 0
 
   for (let index = 0; index < prices.length; index++) {
-    const element = prices[index];
-    const difference = element - min;
+    const element = prices[index]
+    const difference = element - min
     if (difference < 0) {
-      min = element;
+      min = element
     } else {
       if (difference > result) {
-        result = difference;
+        result = difference
       }
     }
   }
 
-  return result;
-};
+  return result
+}
 
-console.log(maxProfit(prices));
+console.log(maxProfit(prices))
+
+//? Outra solução usando ponteiros
+// /**
+//  * @param {number[]} prices
+//  * @return {number}
+//  */
+// var maxProfit = function(prices) {
+
+//     let result=0
+
+//     let left=0
+//     let right=1
+
+//     while(right!==prices.length){
+//         const leftPrice=prices[left]
+//         const rightPrice=prices[right]
+//         if(rightPrice<leftPrice){
+//             left=right
+//             right=left+1
+//             continue
+//         }
+//         result=Math.max((rightPrice-leftPrice),result)
+//         right++
+
+//     }
+
+// return result
+
+// };
