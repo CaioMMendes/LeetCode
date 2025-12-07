83d6bb618cf5dfb3aebf26e5c682db6ede51fcad
Caio Martins Mendes Rodrigues <115363966+CaioMMendes@users.noreply.github.com>
2025-12-07 20:15:37 -0300

    Add 977 All Topics exercises with O(n) time complexity

--- All-Topics/951-1000/977-SquaresofaSortedArray.ts
+++ All-Topics/951-1000/977-SquaresofaSortedArray.ts
@@ -10,3 +10,53 @@ var sortedSquares = function (nums: number[]) {
 }
 
 console.log(sortedSquares(nums))
+
+//? Resolvendo com tempo de complexidade O(n)
+// /**
+//  * @param {number[]} nums
+//  * @return {number[]}
+//  */
+// var sortedSquares = function(nums) {
+
+// let negatives=nums
+// let positiveIndex=null
+
+// for(let i=0;i<nums.length;i++){
+//     if(nums[i]>=0){
+//         negatives=nums.splice(0,i)
+//         positiveIndex=i
+//         break
+//     }
+// }
+
+// negatives=negatives.map(val=>val**2).reverse()
+// if(positiveIndex===null){
+// nums=[]
+// }else{
+// nums=nums.map(val=>val**2)
+// }
+
+// let negativePointer=0
+// let numsPointer=0
+// const result=[]
+
+// while(negativePointer!==negatives.length || numsPointer!==nums.length){
+//     const negativeElement=negatives?.[negativePointer]
+//     const positiveElement=nums?.[numsPointer]
+
+//     if(positiveElement===undefined || (negativeElement<=positiveElement)){
+//         result.push(negativeElement)
+//         negativePointer++
+//         continue
+//     }
+
+//     if(negativeElement===undefined || (positiveElement<negativeElement)){
+//         result.push(positiveElement)
+//         numsPointer++
+//     }
+
+// }
+
+// return result
+
+// };
