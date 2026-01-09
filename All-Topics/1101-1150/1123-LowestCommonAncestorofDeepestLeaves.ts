6a9016ddbef72436397e32e3b50755cb4a57c785
Caio Martins Mendes Rodrigues <115363966+CaioMMendes@users.noreply.github.com>
2026-01-09 10:23:16 -0300

    Add 1123 All Topics exercises

---
+++ All-Topics/1101-1150/1123-LowestCommonAncestorofDeepestLeaves.ts
@@ -0,0 +1,77 @@
+//@ts-nocheck
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var lcaDeepestLeaves = function (root) {
+  const childrenMap = new Map()
+  let deepestValue = [1, [root.val]]
+  let find = null
+
+  runTree(root, 1)
+
+  for (const [key, val] of childrenMap) {
+    let error = false
+
+    for (const deepVal of deepestValue[1]) {
+      if (!val.has(deepVal)) {
+        error = true
+        break
+      }
+    }
+
+    if (error === true) continue
+    find = key
+    break
+  }
+
+  let result = root
+  findResult(root)
+
+  return result
+
+  function findResult(node) {
+    if (node.val === find) result = node
+
+    if (node.left) {
+      findResult(node.left)
+    }
+
+    if (node.right) {
+      findResult(node.right)
+    }
+  }
+
+  function runTree(node, deep) {
+    if (node === null) return []
+
+    if (deep > deepestValue[0]) {
+      deepestValue = [deep, [node.val]]
+    } else if (deep === deepestValue[0]) {
+      deepestValue[1] = [...deepestValue[1], node.val]
+    }
+
+    const leftChildren = runTree(node.left, deep + 1)
+    const rightChildren = runTree(node.right, deep + 1)
+
+    const allChildren = new Set([
+      node.val,
+      ...(node.left ? [node.left.val] : []),
+      ...leftChildren,
+      ...(node.right ? [node.right.val] : []),
+      ...rightChildren,
+    ])
+
+    childrenMap.set(node.val, allChildren)
+    return allChildren
+  }
+}
