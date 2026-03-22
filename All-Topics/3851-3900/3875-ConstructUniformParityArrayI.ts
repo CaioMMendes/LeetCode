d7a44a3267c9972dc71eee1d8463eea265c2367c
Caio Martins Mendes Rodrigues <115363966+CaioMMendes@users.noreply.github.com>
2026-03-22 13:01:29 -0300

    Add 3875 All Topics exercises

---
+++ All-Topics/3851-3900/3875-ConstructUniformParityArrayI.ts
@@ -0,0 +1,11 @@
+const nums1 = [2, 3]
+
+/**
+ * @param {number[]} nums1
+ * @return {boolean}
+ */
+var uniformArray = function (nums1: number[]) {
+  return true
+}
+
+console.log(uniformArray(nums1))
