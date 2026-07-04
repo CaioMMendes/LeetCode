649a26ce0cca8b59db1c66b08beba5fc6b35f23b
Caio Martins Mendes Rodrigues <115363966+CaioMMendes@users.noreply.github.com>
2026-07-04 18:42:08 -0300

    Update 2391 types

--- All-Topics/2351-2400/2391-MinimumAmountofTimetoCollectGarbage.ts
+++ All-Topics/2351-2400/2391-MinimumAmountofTimetoCollectGarbage.ts
@@ -1,58 +1,29 @@
 type GarbageChar = "P" | "M" | "G"
 type CountMap = Partial<Record<GarbageChar, number>>
 
-const garbage = ["G", "P", "GP", "GG"],
-  travel = [2, 4, 3]
-
-/**
- * @param {string[]} garbage
- * @param {number[]} travel
- * @return {number}
- */
-var garbageCollection = function (garbage: string[], travel: number[]) {
-  let total = 0
-  let obj: Record<number, CountMap> = {}
-
-  let result = 0
-
-  const lastGarbage: Partial<Record<GarbageChar, number>> = {
-    "G": 0,
-    "P": 0,
-    "M": 0,
-  }
+const garbageCollection = (garbage: string[], travel: number[]): number => {
+  const obj: CountMap[] = []
+  const lastGarbage: Record<GarbageChar, number> = { P: 0, M: 0, G: 0 }
 
   for (let i = 0; i < garbage.length; i++) {
-    if (!obj[i]) obj[i] = {}
+    obj[i] = {}
     for (const char of garbage[i] as Iterable<GarbageChar>) {
-      if (obj?.[i]?.[char]) {
-        obj[i][char]++
-      } else {
-        obj[i][char] = 1
-      }
+      obj[i][char] = (obj[i][char] ?? 0) + 1
       lastGarbage[char] = i
-      total++
     }
   }
 
-  //truck P
-  for (let i = 0; i <= lastGarbage["P"]; i++) {
-    result += travel?.[i - 1] ?? 0
-    result += obj[i]["P"] ?? 0
-  }
+  let result = 0
+  const trucks: GarbageChar[] = ["P", "M", "G"]
 
-  //truck M
-  for (let i = 0; i <= lastGarbage["M"]; i++) {
-    result += travel?.[i - 1] ?? 0
-    result += obj[i]["M"] ?? 0
-  }
-
-  //truck G
-  for (let i = 0; i <= lastGarbage["G"]; i++) {
-    result += travel?.[i - 1] ?? 0
-    result += obj[i]["G"] ?? 0
+  for (const truck of trucks) {
+    for (let i = 0; i <= lastGarbage[truck]; i++) {
+      result += travel[i - 1] ?? 0
+      result += obj[i]?.[truck] ?? 0
+    }
   }
 
   return result
 }
 
-console.log(garbageCollection(garbage, travel))
+console.log(garbageCollection(["G", "P", "GP", "GG"], [2, 4, 3])) // 21
